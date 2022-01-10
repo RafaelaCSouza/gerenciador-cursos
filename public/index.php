b651537a569ac2ddf9ed3f53c78c8f1be92b9c85
Rafaela Cristina <rafaela.cristina1610@gmail.com>
2022-01-10 17:45:31 -0300

    ajuste de rota

--- public/index.php
+++ public/index.php
@@ -5,15 +5,15 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
-$caminho = $_SERVER['REQUEST_URI'];
+$caminho = parse_url($_SERVER['REQUEST_URI']);
 $rotas = require __DIR__ . '/../config/routes.php';
 
-if (!array_key_exists($caminho, $rotas)) {
+if (!array_key_exists($caminho['path'], $rotas)) {
     http_response_code(404);
     exit();
 }
 
-$classeControladora = $rotas[$caminho];
+$classeControladora = $rotas[$caminho['path']];
 /** @var InterfaceControladorRequisicao $controlador */
 $controlador = new $classeControladora();
 $controlador->processaRequisicao();
--- view/cursos/listar-cursos.php
+++ view/cursos/listar-cursos.php
@@ -7,9 +7,6 @@
         <?php foreach ($cursos as $curso) : ?>
             <li class="list-group-item d-flex justify-content-between">
                 <?= $curso->getDescricao(); ?>
-
-                <span><?=$curso->getId();?></span>
-
                 <a href="/excluir-curso?id=<?=$curso->getId();?>" class="btn btn-danger btn-sm">
                         Excluir
                 </a>
